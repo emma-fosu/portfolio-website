508ed3965c085e8c14aaaf5594f1fb85abe24dea
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-11-13 11:03:52 +0000

    Setup HTML and Routes

---
+++ src/scripts/pages/projects/cossmic_electricity_analysis.ts
@@ -0,0 +1,2 @@
+import "../../../styles/tailwind.css";
+import "../../../styles/pages/projects/cossmic_electricity_analysis.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import {resolve} from "node:path";
 
 export default defineConfig({
   base: "/portfolio-website/",
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "projects/index.html")
+      }
+    }
+  }
 });
